95799c766adc95f472193e899e0e66ff57cf145e
Noah12398 <149400947+Noah12398@users.noreply.github.com>
2024-07-07 01:30:16 +0530

    Update App.js

--- giftlink-frontend/src/App.js
+++ giftlink-frontend/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Routes, Route, useNavigate } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import MainPage from './components/MainPage/MainPage';
 import LoginPage from './components/LoginPage/LoginPage';
 import RegisterPage from './components/RegisterPage/RegisterPage';
@@ -9,20 +9,20 @@ import Navbar from './components/Navbar/Navbar';
 import DetailsPage from './components/DetailsPage/DetailsPage';
 import SearchPage from './components/SearchPage/SearchPage';
 import Profile from './components/Profile/Profile';
+
 function App() {
   return (
-        <>
-        <Navbar/>
-        <Routes>
-          <Route path="/" element={<MainPage />} />
-          <Route path="/app" element={<MainPage />} />
-          <Route path="/app/login" element={<LoginPage/>} />
-          <Route path="/app/register" element={<RegisterPage />} />
-          <Route path="/app/product/:productId" element={<DetailsPage/>} />
-          <Route path="/app/search" element={<SearchPage/>} />
-          <Route path="/app/profile" element={<Profile/>} />
-        </Routes>
-        </>
+      <Routes>
+        <Route path="/" element={<MainPage />} />
+        <Route path="/app" element={<MainPage />} />
+        <Route path="/app/login" element={<LoginPage />} />
+        <Route path="/app/register" element={<RegisterPage />} />
+        <Route path="/app/product/:productId" element={<DetailsPage />} />
+        <Route path="/app/search" element={<SearchPage />} />
+        <Route path="/app/profile" element={<Profile />} />
+      </Routes>
+    </>
   );
 }
+
 export default App;
